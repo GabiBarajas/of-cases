6af1255294c8b40da27dc8999389a28b664071ba
Niels E. Linnemann Nielsen <ne.linnemann@gmail.com>
2016-03-18 12:29:05 +0100

    Update runMeshCase.sh

--- incompressible/simpleFoam/Ahmed-Body-25degree/runMeshCase.sh
+++ incompressible/simpleFoam/Ahmed-Body-25degree/runMeshCase.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# Use cfmesh here #
+
 surfaceToFMS joined.stl
 
 surfaceFeatureEdges -angle 80 joined.fms joined2.fms
 
+cartesianMesh
+
+# Use cfmesh or OF here
+
 transformPoints -scale "(0.001 0.001 0.001)"
+
+# Use OF here
 
 createPatch -overwrite
 
